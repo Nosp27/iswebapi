01341d264fea2b1cf8426f55a15529c7ede56c3a
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2019-10-26 14:33:11 +0300

    create project template

---
+++ src/main/java/com/ashikhmin/iswebapi/IswebapiApplication.java
@@ -0,0 +1,13 @@
+package com.ashikhmin.iswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IswebapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IswebapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ashikhmin/iswebapi/IswebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashikhmin.iswebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IswebapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
