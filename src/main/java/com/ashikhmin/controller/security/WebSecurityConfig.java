ab43011c646f381357267c65cbec8c6ac32ea488
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2019-12-05 23:08:10 +0300

    disable security

--- src/main/java/com/ashikhmin/controller/security/WebSecurityConfig.java
+++ src/main/java/com/ashikhmin/controller/security/WebSecurityConfig.java
@@ -10,6 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        getHttp().csrf().disable();
     }
 }
