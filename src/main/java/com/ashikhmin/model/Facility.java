41522aa35fc07c4f69ba6cb184f9f9b29d92baa4
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-02-14 19:49:03 +0300

    Add new columns to model

--- src/main/java/com/ashikhmin/model/Facility.java
+++ src/main/java/com/ashikhmin/model/Facility.java
@@ -6,7 +6,9 @@ import org.hibernate.annotations.Cascade;
 import javax.persistence.Id;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -16,13 +18,13 @@ public class Facility implements HasImage {
     private Integer _id;
     private String name;
     private String description;
-
-    @Column(columnDefinition = "double precision not null default 0")
     private Double lat;
-
-    @Column(columnDefinition = "double precision not null default 0")
     private Double lng;
     private Integer imageId;
+    private String utility = "Unavailable";
+    private Integer employees;
+    private Double investmentSize;
+    private Double profitability;
 
     public Facility() {
         _id = 0;
@@ -30,6 +32,44 @@ public class Facility implements HasImage {
         lng = 0.0;
     }
 
+    @ManyToMany
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @JoinTable(
+            name = "facility_to_category",
+            joinColumns = @JoinColumn(name = "_id"),
+            inverseJoinColumns = @JoinColumn(name = "cat_id"))
+    private Set<Category> categories;
+
+    @ManyToOne
+    @JoinTable(
+            name = "facility_to_region",
+            joinColumns = @JoinColumn(name = "_id"),
+            inverseJoinColumns = @JoinColumn(name = "regionId"))
+    private Region region;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "favoriteFacilities")
+    private Set<Actor> subscribedActors;
+
+    public Set<Actor> getSubscribedActors() {
+        if (subscribedActors == null)
+            subscribedActors = new HashSet<>();
+        return subscribedActors;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!obj.getClass().equals(getClass()))
+            return false;
+        return get_id().equals(((Facility) obj).get_id());
+    }
+
+    @Override
+    public int hashCode() {
+        return get_id().hashCode();
+    }
+
+    //<editor-fold desc="Accessors">
     public void setLat(Double lat) {
         this.lat = lat;
     }
@@ -91,56 +131,60 @@ public class Facility implements HasImage {
         this.region = region;
     }
 
-    @ManyToMany
-    @Cascade(org.hibernate.annotations.CascadeType.ALL)
-    @JoinTable(
-            name = "facility_to_category",
-            joinColumns = @JoinColumn(name = "_id"),
-            inverseJoinColumns = @JoinColumn(name = "cat_id"))
-    private Set<Category> categories;
-
-    @ManyToOne
-    @JoinTable(
-            name = "facility_to_region",
-            joinColumns = @JoinColumn(name = "_id"),
-            inverseJoinColumns = @JoinColumn(name = "regionId"))
-    private Region region;
-
     public Double getLat() {
         return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
     }
 
     public Double getLng() {
         return lng;
     }
 
-    public void setLng(double lng) {
-        this.lng = lng;
+    public String getUtility() {
+        return utility;
     }
 
-    @JsonIgnore
-    @ManyToMany(mappedBy = "favoriteFacilities")
-    private Set<Actor> subscribedActors;
 
-    public Set<Actor> getSubscribedActors() {
-        if (subscribedActors == null)
-            subscribedActors = new HashSet<>();
-        return subscribedActors;
+    private static final List<String> POSSIBLE_UTILITIES = Arrays.asList(
+            "Available",
+            "Unavailable",
+            "Under Construction"
+    );
+
+    public void setUtility(String utility) {
+        if (!POSSIBLE_UTILITIES.contains(utility))
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Unavailable value for utility: %s." +
+                                    "available values: %s",
+                            utility,
+                            Arrays.toString(POSSIBLE_UTILITIES.toArray())
+                    )
+            );
+        this.utility = utility;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (!obj.getClass().equals(getClass()))
-            return false;
-        return get_id().equals(((Facility) obj).get_id());
+    public Integer getEmployees() {
+        return employees;
     }
 
-    @Override
-    public int hashCode() {
-        return get_id().hashCode();
+    public void setEmployees(Integer employees) {
+        this.employees = employees;
     }
+
+    public Double getInvestmentSize() {
+        return investmentSize;
+    }
+
+    public void setInvestmentSize(Double investmentSize) {
+        this.investmentSize = investmentSize;
+    }
+
+    public Double getProfitability() {
+        return profitability;
+    }
+
+    public void setProfitability(Double profitability) {
+        this.profitability = profitability;
+    }
+    //</editor-fold>
 }
--- src/main/java/com/ashikhmin/model/Region.java
+++ src/main/java/com/ashikhmin/model/Region.java
@@ -13,6 +13,11 @@ public class Region implements HasImage {
     private Integer imageId;
     private Double area;
     private Integer population;
+    private Integer unemployed;
+    private Integer totalLabourForce;
+    private Double gdp;
+    private Double avgPropertyPrice;
+    private Double avgFamilyIncome;
 
     // <editor-fold desc = "Accessors">
     public Integer getImageId() {
@@ -53,6 +58,46 @@ public class Region implements HasImage {
 
     public void setPopulation(Integer population) {
         this.population = population;
+    }
+
+    public Integer getUnemployed() {
+        return unemployed;
+    }
+
+    public void setUnemployed(Integer unemployed) {
+        this.unemployed = unemployed;
+    }
+
+    public Integer getTotalLabourForce() {
+        return totalLabourForce;
+    }
+
+    public void setTotalLabourForce(Integer totalLabourForce) {
+        this.totalLabourForce = totalLabourForce;
+    }
+
+    public Double getGdp() {
+        return gdp;
+    }
+
+    public void setGdp(Double gdp) {
+        this.gdp = gdp;
+    }
+
+    public Double getAvgPropertyPrice() {
+        return avgPropertyPrice;
+    }
+
+    public void setAvgPropertyPrice(Double avgPropertyPrice) {
+        this.avgPropertyPrice = avgPropertyPrice;
+    }
+
+    public Double getAvgFamilyIncome() {
+        return avgFamilyIncome;
+    }
+
+    public void setAvgFamilyIncome(Double avgFamilyIncome) {
+        this.avgFamilyIncome = avgFamilyIncome;
     }
     // </editor-fold>
 }
