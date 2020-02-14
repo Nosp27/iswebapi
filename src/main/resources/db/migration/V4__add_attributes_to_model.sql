7a742c3305561386ac60cc04d3d43232456a3f2d
Pavel Ashikhmin <p.ashikhmin@rambler-co.ru>
2020-02-14 22:27:15 +0300

    Edit migration: No extra type

--- src/main/resources/db/migration/V4__add_attributes_to_model.sql
+++ src/main/resources/db/migration/V4__add_attributes_to_model.sql
@@ -4,13 +4,8 @@ ALTER TABLE region
     ADD COLUMN IF NOT EXISTS gdp double precision,
     ADD COLUMN IF NOT EXISTS avg_property_price double precision,
     ADD COLUMN IF NOT EXISTS avg_family_income double precision;
-CREATE TYPE utility_type AS ENUM (
-    'Available',
-    'Under Construction',
-    'Unavailable'
-);
 ALTER TABLE facility
-    ADD COLUMN IF NOT EXISTS utility utility_type NOT NULL DEFAULT 'Unavailable',
+    ADD COLUMN IF NOT EXISTS utility text NOT NULL DEFAULT 'Unavailable',
     ADD COLUMN IF NOT EXISTS employees int,
     ADD COLUMN IF NOT EXISTS investment_size double precision,
     ADD COLUMN IF NOT EXISTS profitability double precision;
